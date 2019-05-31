5b361b53bd370c19a979c6b4157cb160ec377bf1
Gin <hyacinthhome@outlook.com>
2019-05-31 20:04:14 +0800

    hehehe

---
+++ src/main/java/cn/itcast/entity/Que.java
@@ -0,0 +1,14 @@
+package cn.itcast.entity;
+
+public class Que {
+    String que_id;
+    String que_des;
+    String que_choa;
+    String que_chob;
+    String que_choc;
+    String que_chod;
+    String que_type;
+    String que_source;
+
+    
+}
